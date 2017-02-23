a532219b69a8305fc1260c06c61b541ae691fb25
thinkholic <mailtokasun@gmail.com>
2017-02-24 00:29:34 +0530

    Change some variable names

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,5 @@
 var chai = require('chai');  
-var assert = chai.assert;    // Using Assert style
-var expect = chai.expect;    // Using Expect style
-var should = chai.should();  // Using Should style
+var expect = chai.expect;
 
 require('../lib');
 
@@ -10,13 +8,13 @@ describe("string-prototype", function() {
     var str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
     var strExpected = 'Lorem REPLACED-ipsum dolor sit REPLACED-amet, consectetur adipiscing elit.';
 
-    var list = {
-        'ipsum': 'REPLACED-ipsum',
-        'amet': 'REPLACED-amet'
+    var params = {
+      'ipsum': 'REPLACED-ipsum',
+      'amet': 'REPLACED-amet'
     };
 
     it("should have replace all instance at the list with the string provided", function() {
-      var newStr = str.allReplace(list);
+      var newStr = str.allReplace(params);
 
       expect(newStr).to.equal(strExpected);
     });
